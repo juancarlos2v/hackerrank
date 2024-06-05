c1a8739dc8790f571cc015dd7f5375f5c7988648
Juan Carlos Vilcherrez <juanc.vilcherrez@gmail.com>
2024-06-05 15:48:17 -0300

    Map Query

--- src/JavaPreparation/JavaMap.java
+++ src/JavaPreparation/JavaMap.java
@@ -1,41 +1,36 @@
 package JavaPreparation;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class JavaMap {
     public static void main(String[] args) throws Exception {
-        Scanner in = new Scanner(System.in);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         Map<String, Integer> phonebook = new HashMap<>();
-        List<String> result = new ArrayList<>();
 
-        int n = in.nextInt();
-        in.nextLine();
-        for (int i = 0; i < n; i++) {
-            String name = in.nextLine();
-            int phone = in.nextInt();
+        int contacts = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < contacts; i++) {
+            String name = br.readLine();
+            int phone = Integer.parseInt(br.readLine());
             phonebook.put(name, phone);
-            in.nextLine();
         }
-        //System.out.println(phonebook);
 
-        while (in.hasNext()) {
-            String query = in.nextLine();
-
-            for (String key : phonebook.keySet()) {
-                if (key.equals(query)) {
-                    //System.out.println( key + "=" + phonebook.get(query));
-                    result.add(key + "=" + phonebook.get(query));
-                } else {
-                    System.out.println("Not Found");
-                }
+        while (true) {
+            String query = br.readLine();
+            if (query == null || query.isEmpty()) {
+                break;
             }
-        }
-        for (String r:result
-             ) {
-            System.out.println(r);
-        }
+            if (phonebook.containsKey(query)) {
+                System.out.println(query + "=" + phonebook.get(query));
+            } else {
+                System.out.println("Not Found");
+            }
 
-
+        }
     }
 
 }
